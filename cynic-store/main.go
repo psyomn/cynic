789701c9a7eebd221153e272962b7cfd1c05fdc1
Simon Symeonidis <lethaljellybean@gmail.com>
2021-04-03 01:06:20 -0400

    Update copyright dates on cynic-store

--- cynic-store/main.go
+++ cynic-store/main.go
@@ -1,8 +1,7 @@
 /*
 Use this to do simple dumps of cynic-storage files.
 
-Copyright 2018 Simon Symeonidis (psyomn)
-Copyright 2019 Simon Symeonidis (psyomn)
+Copyright 2018-2021 Simon Symeonidis (psyomn)
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
